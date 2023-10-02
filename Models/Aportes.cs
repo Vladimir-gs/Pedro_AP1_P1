300f43c6c7b66f1bd9d5394f796141e0eb370204
Vladimir-gs <pedro_guzman1@ucne.edu.do>
2023-10-02 19:29:32 -0300

    Agregue mi Clase aporte

---
+++ Models/Aportes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Pedro_AP1_P1.Models
+{
+    public class Aportes {
+        [Key]
+        public int AportesId { get; set; }
+        [Required(ErrorMessage ="Debe ingresar el Nombre de la persona")]
+        public string Persona { get; set; }
+        [Required(ErrorMessage ="Agrege una onservacion")]
+        public string Observacion { get; set; }
+        [Required(ErrorMessage ="Agrege un monto")]
+        public int Monto { get; set; }
+    }
+}
