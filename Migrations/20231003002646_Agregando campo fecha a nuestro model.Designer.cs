eb4bb793bd6b80853117be5ad75495586a88f40a
Vladimir-gs <pedro_guzman1@ucne.edu.do>
2023-10-02 22:10:23 -0300

    Update de la base de datos

--- Migrations/20231003002646_Agregando campo fecha a nuestro model.Designer.cs
+++ Migrations/20231003002646_Agregando campo fecha a nuestro model.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Pedro_AP1_P1.DAL;
 
@@ -10,9 +11,11 @@ using Pedro_AP1_P1.DAL;
 namespace Pedro_AP1_P1.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20231003002646_Agregando campo fecha a nuestro model")]
+    partial class Agregandocampofechaanuestromodel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.11");
---
+++ Migrations/20231003002646_Agregando campo fecha a nuestro model.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pedro_AP1_P1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Agregandocampofechaanuestromodel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Fecha",
+                table: "Aportes",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Fecha",
+                table: "Aportes");
+        }
+    }
+}
