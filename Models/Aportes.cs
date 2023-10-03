37e73c192872c032282affa03ce93ca13747c896
Vladimir-gs <pedro_guzman1@ucne.edu.do>
2023-10-02 22:10:56 -0300

    Agregamos el dato Fecha en nuestra clase Aportes

--- Models/Aportes.cs
+++ Models/Aportes.cs
@@ -7,6 +7,7 @@ namespace Pedro_AP1_P1.Models
         [Key]
         public int AportesId { get; set; }
         [Required(ErrorMessage ="Debe ingresar el Nombre de la persona")]
+        public DateTime Fecha{get; set;} = DateTime.Now;
         public string Persona { get; set; }
         [Required(ErrorMessage ="Agrege una onservacion")]
         public string Observacion { get; set; }
